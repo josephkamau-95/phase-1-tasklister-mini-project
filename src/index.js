5f2296f554417a3c36b36ac0579667d2f934736a
Joseph Mwangi <josymuramu@gmail.com>
2025-04-26 07:29:40 +0300

    completed the assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const formInput = document.getElementById("new-task-description")
+  const submitButton = document.querySelector("input[type = 'submit'][value = 'Create New Task']")
+  submitButton.id = "submit-button"
+  const taskList = document.querySelector("#tasks")
+  
+
+  submitButton.addEventListener("click", function(event){
+      event.preventDefault()
+      const value = formInput.value.trim()
+      if (value !== ''){
+        const liElement = document.createElement("li")
+        liElement.textContent = value
+        taskList.appendChild(liElement)
+        formInput.value = ''
+      }
+  })
 });
+
+
